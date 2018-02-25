f4f6ac9e3f31b424a0e60a25d2048a0f5676538b
Sustavov Anton <sustav86@gmail.com>
2018-02-25 17:48:58 +0200

    Create rest get method

--- src/main/java/ua/sustavov/gateway/gateway/controller/TransactionController.java
+++ src/main/java/ua/sustavov/gateway/gateway/controller/TransactionController.java
@@ -1,10 +1,14 @@
 package ua.sustavov.gateway.gateway.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ua.sustavov.gateway.gateway.dto.TransactionDto;
+import ua.sustavov.gateway.gateway.mapper.QueryStringMapper;
 import ua.sustavov.gateway.gateway.service.*;
+
+import javax.ws.rs.BeanParam;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.OK;
@@ -13,17 +17,27 @@ import static org.springframework.http.HttpStatus.OK;
 public class TransactionController {
 
     private final BusinessService businessService;
+    private final ObjectMapper objectMapper;
 
     @Autowired
-    public TransactionController(BusinessService businessService) {
+    public TransactionController(BusinessService businessService,
+                                 ObjectMapper objectMapper) {
         this.businessService = businessService;
+        this.objectMapper = objectMapper;
     }
 
     @ResponseStatus(OK)
-    @RequestMapping(value = "/api", consumes = {"application/x-www-form-urlencoded", "text/xml", "text/plain"}, method = {RequestMethod.POST})
-    public ResponseEntity<?> createTransactionString(@RequestBody TransactionDto transactionDto) {
+    @RequestMapping(value = "/api", method = {RequestMethod.GET})
+    public ResponseEntity<?> createTransactionString(@BeanParam QueryStringMapper queryStringMapper) {
 
+        TransactionDto transactionDto = objectMapper.convertValue(queryStringMapper, TransactionDto.class);
+
+        transactionDto = businessService.performTransaction(transactionDto);
+        if (transactionDto == null) {
+            return new ResponseEntity<>("Failed to create new Transaction using given data.", BAD_REQUEST);
+        }
         return new ResponseEntity<>(transactionDto, OK);
+
     }
 
     @ResponseStatus(OK)
